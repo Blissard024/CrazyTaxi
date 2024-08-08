071dadca2916149bf6e209d6abf876b5eb0816f3
DK\ara <ara@sitecore.net>
2024-08-08 18:03:57 -0400

    Add Change Color

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    [SerializeField] float  DestroyDelay = 0.1f;  
+    [SerializeField] float  DestroyDelay = 0.1f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,1);
+    [SerializeField] Color32 noPackagecolor = new Color32(1,1,1,1); 
     bool hasPackage = false;
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
         
         if(other.tag == "Package" & hasPackage == false )
         {
             Debug.Log("Packaged picked up");
             Destroy(other.gameObject,DestroyDelay);
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            
         }
 
         if(other.tag == "Station" & hasPackage == true )
@@ -22,6 +27,7 @@ public class Delivery : MonoBehaviour
             Debug.Log("Packaged delivered");
             Destroy(other.gameObject,DestroyDelay);
             hasPackage = false;
+            spriteRenderer.color = noPackagecolor;
 
         }
 
